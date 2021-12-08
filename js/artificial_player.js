dfa6e2d7ebc24688aff84ae17666cc12a1632683
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2021-12-08 16:55:27 -0500

    slightly less ridiculous deterministic strategy

--- js/artificial_player.js
+++ js/artificial_player.js
@@ -2,8 +2,16 @@ function ArtificialPlayer() {
   console.log(0);
 }
 
-ArtificialPlayer.prototype.move = function () {
-  // 0: up, 1: right, 2: down, 3: left
-  console.log(1);
+// 0: up, 1: right, 2: down, 3: left
+ArtificialPlayer.prototype.move = deterministic1;
+
+function deterministic1() {
+  r = Math.random();
+  if (r < 0.5) { return 2; }
+  if (r < 0.9) { return 1; }
+  return 3;
+}
+
+function alwaysDown() {
   return 2;
-};
+}
